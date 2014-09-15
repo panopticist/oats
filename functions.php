6f32cc12188456111540b7d93814887a9c29a401
Jeremy Zilar <jeremyzilar@gmail.com>
2014-09-15 12:31:19 -0400

    Fixing the relative path to the INC folder

--- functions.php
+++ functions.php
@@ -9,17 +9,12 @@ $v = '1';
 define(V, $v);
 
 
-
-
-
 // Include Path
 // All template files go in the /INC/ folder. 
 // Use this to call those files —
 // include_once( INC . 'filename.php' );
-$inc = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"] . 'inc/';
+$inc = $_SERVER["DOCUMENT_ROOT"] . dirname($_SERVER["PHP_SELF"]) . '/inc/';
 define(INC, $inc);
-
-
 
 
 ?>
