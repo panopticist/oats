8d5eb8cc6806446bf9ee7dec8efe3c63839d76fe
panopticist <hearst@gmail.com>
2014-09-15 11:52:48 -0400

    Monday experiments

--- inc/head.php
+++ inc/head.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <h1><i class="fa fa-bolt"></i> Foundation <span>beta</span></h1>
+        <h1><i class="fa fa-bolt"></i> OATS <span>beta</span></h1>
       </div>
     </div>
   </div>
