85fb2bf80afd5b5e73f330bd64cb6acfe95048fa
Jeremy Zilar <jeremyzilar@gmail.com>
2014-09-21 22:16:45 -0400

    Featured images

--- functions.php
+++ functions.php
@@ -53,6 +53,15 @@ function featured_image($post){
 }
 
 
+// Strips out the width and height
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3 );
+
+function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
+
+
 
 // Example Functions
 function hearst(){
