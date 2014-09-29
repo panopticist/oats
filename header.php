af1be2ce3e0ee642b43430d5ce5c3ac74ddd6862
panopticist <hearst@gmail.com>
2014-09-28 20:38:40 -0400

    Sunday tweaks

--- header.php
+++ header.php
@@ -51,12 +51,66 @@
     <header>
       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
-            <h1>
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo TDIR . '/img/graphics/oats-logo-blue.svg'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class=" img-responsive logo"></a>
-            </h1>
 
-            <?php include( INC . 'primary-nav.php' ); ?>
+    <!-- PRIMARY-NAV MODULE --> 
+					<div class="col-xs-12">
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo TDIR . '/img/graphics/oats-logo-blue.svg'; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" class=" img-responsive logo"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+			  <li><a href="<?php echo get_site_url(); ?>/approach/">Approach</a></li>
+			  <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Work <span class="caret"></a>
+			    <ul class="dropdown-menu" role="menu">
+			      <li><a href="<?php echo get_site_url(); ?>/flagship-programs/">Flagship Programs</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/client-projects/">Client Projects</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/white-papers/">White Papers</a></li>
+			    </ul>
+			  </li>
+			  <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">About <span class="caret"></a>
+			    <ul class="dropdown-menu" role="menu">
+			      <li><a href="<?php echo get_site_url(); ?>/team/">Team</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/board/">Board</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/partners/">Partners</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/program-sites/">Program Sites</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/funders/">Funders</a></li>
+			      <li><a href="<?php echo get_site_url(); ?>/in-kind-donors/">In-Kind Donors</a></li>
+			    </ul>
+			  </li>
+			  <li><a href="<?php echo get_site_url(); ?>/press/">Press</a></li>
+			  <li><a href="<?php echo get_site_url(); ?>/contact/">Contact Us</a></li>
+			  <li><a href="<?php echo get_site_url(); ?>/donate/">Donate</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<!-- OLD PRIMARY-NAV MODULE
+
+<ul class="nav nav-pills">
+</ul>
+
+ --> 
+
+
+					</div>
+				
+
+            <?php // include( INC . 'primary-nav.php' ); ?>
 
           </div>
         </div>
