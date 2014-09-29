ad1bbcb5572d921812e640ade9926869307deb32
panopticist <hearst@gmail.com>
2014-09-29 14:16:07 -0400

    Press archive tweaks

--- archive-press.php
+++ archive-press.php
@@ -31,24 +31,32 @@
 
 	  </div>
 	
-	</div>
-
-	<div class="row">
-
-	  <div class="col-xs-12 col-md-10">
-
-			<p class="lead">
-				<?php
-				$post_id = 85;
-				$queried_post = get_post($post_id);
-				echo $queried_post->post_content;
-				?>
-			</p>
-
-	  </div>
+		</div>
 	
-	</div>
+		<div class="row">
+	
+		  <div class="col-xs-12 col-md-10">
+	
+				<p class="lead">
+					<?php
+					$post_id = 85;
+					$queried_post = get_post($post_id);
+					echo $queried_post->post_content;
+					?>
+				</p>
+	
+		  </div>
+		
+		</div>
 
+	</div>
+</section>
+
+
+<section class="tout-grid">
+	<div class="container">
+
+		<div class="row">
 			<?php 
 				$the_query = new WP_Query('post_type=press', 'posts_per_page=-1');
 
@@ -57,36 +65,35 @@
 					while ( $the_query->have_posts() ) {
 						$the_query->the_post(); ?>
 
-		<div class="row newsfeed">
+						<?php if ($i % 5 == 0){
+							echo "</div><div class='row'>";
+						} ?>
+							
+						<div class="col-xs-12 col-sm-6 col-md-3">
 
-						  <div class="col-xs-12 col-sm-3">
-						      
-		      	      <!--<img src="http://placehold.it/400x250" style="width: 100%; height: 100%;">-->
+					    	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'medium'); ?></a>
 
-						      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'medium'); ?></a>
-						      
-						  </div>
+				        <h4 class="list"><?php the_time(get_option('date_format')); ?></h4>
+				        
+				        <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 
-						  <div class="col-xs-12 col-sm-8">
-						      <h4 class="list"><?php the_time(get_option('date_format')); ?></h4>
-						      <h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+				        <p class="entry-summary">
+									<?php the_excerpt(); ?>
+								</p>
 
-						        <!--<p class="entry-summary">
-											<?php the_excerpt(); ?>
-										</p>-->
+				        <p><a href="<?php the_permalink(); ?>" class="btn btn-primary" role="button">Learn More</a></p>
 
-						  </div>
-
-		</div>
+					  </div>
 
 					<?php 
 					$i++;
 					}
 				}
 			?>
+		</div>
 
-
-</div>
+	</div>
 </section>
+
 
 <?php include('footer.php'); ?>
